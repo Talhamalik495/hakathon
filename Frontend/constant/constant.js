1eaf7e433bdd9cba6705eb54076c6625776322be
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2025-01-26 07:43:04 +0500

    update

--- Frontend/constant/constant.js
+++ Frontend/constant/constant.js
@@ -1,4 +1,5 @@
-let prodUrl = "http://localhost:4000/";
+let devUrl = "http://localhost:4000/";
+let prodUrl = "https://hakathon-euqf.vercel.app/";
 
 let routers = {
   genrateToken: prodUrl + "tokenGenrate",
